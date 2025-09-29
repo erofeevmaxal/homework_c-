d8953768668afe83cbf20cd5e1b91671ee5aeccc
ErofeevMaxal <erofeevmaxal@gmail.com>
2025-09-29 22:09:52 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("температура в градусах Цельсия: ");
+            string input = Console.ReadLine();
+            float degreesCelsius = float.Parse(input);
+            float degreesFahrenheit = degreesCelsius * 1.8f + 32;
+            Console.WriteLine($"температура в градусах Фаренгейта: {degreesFahrenheit}");
+        }
+    }
+}
