6c1726c7efc2bfcc726da290c0735a6d883322ac
ErofeevMaxal <erofeevmaxal@gmail.com>
2025-09-30 14:27:07 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводите числа по одному. Для завершения ввидите 0.");
+            float sum = 0;
+
+            while (true)
+            {
+                if (!float.TryParse(Console.ReadLine(), out float number))
+                {
+                    Console.WriteLine("Ошибка: введите корректное число.");
+                    continue;
+                }
+
+                if (number == 0)
+                {
+                    break;
+                }
+
+                sum += number;
+                Console.WriteLine($"Текущая сумма: {sum}");
+            }
+        }
+    }
+}
