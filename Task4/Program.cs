eabd94af2d82eebd941dd6510675eb6b810c9f90
ErofeevMaxal <erofeevmaxal@gmail.com>
2025-09-30 14:39:59 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пароль для проверки:");
+            string password = Console.ReadLine();
+            bool isValid = true;
+
+            if (password.Length < 8)
+            {
+                Console.WriteLine("длина должна быть не менее 8 символов.");
+                isValid = false;
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                Console.WriteLine("Пароль должен содержать хотя бы одну цифру.");
+                isValid = false;
+            }
+
+            if (!password.Any(char.IsUpper))
+            {
+                Console.WriteLine("Пароль должен содержать хотя бы одну заглавную букву.");
+                isValid = false;
+            }
+
+            if (!password.Any(c => "!@#$%^&*".Contains(c)))
+            {
+                Console.WriteLine("Пароль должен содержать хотя бы один специальный символ из !@#$%^&*.");
+                isValid = false;
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine("Пароль соответствует всем критериям!");
+            }
+        }
+    }
+}
